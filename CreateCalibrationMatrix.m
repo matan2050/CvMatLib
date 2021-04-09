13e89256ff1ff99cfd05167209865643a8f916ef
Matan Weissbuch <matan2050@gmail.com>
2021-04-09 23:03:01 +0300

    added comments, fix rotation matrix from y-p-r to x-y-z

--- CreateCalibrationMatrix.m
+++ CreateCalibrationMatrix.m
@@ -1,8 +1,8 @@
 function [K] = CreateCalibrationMatrix(fx, fy, cx, cy)
   K = zeros(3,3);
-  K(1,1) = fx;
-  K(2,2) = fy;
-  K(1,3) = cx;
-  K(2,3) = cy;
+  K(1,1) = fx;  # focal length x (in px)
+  K(2,2) = fy;  # focal length y (in px)
+  K(1,3) = cx;  # image center x (in px)
+  K(2,3) = cy;  # image center y (in px)
   K(3,3) = 1;
 end
